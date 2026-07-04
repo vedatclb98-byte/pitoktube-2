31a0d06bce8defa52c601ac862c65edba14ce4ea
vedatclb98-byte <299308558+vedatclb98-byte@users.noreply.github.com>
2026-07-04 17:09:15 +0300

    feat: create vertical video feed

--- lib/features/feed/feed_screen.dart
+++ lib/features/feed/feed_screen.dart
@@ -6,21 +6,70 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text("PitokTube"),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text(
-          "Video Feed",
-          style: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Stack(
+            fit: StackFit.expand,
+            children: [
+
+              Container(
+                color: Colors.grey.shade900,
+              ),
+
+              Center(
+                child: Text(
+                  "Video ${index + 1}",
+                  style: const TextStyle(
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              Positioned(
+                right: 15,
+                bottom: 120,
+                child: Column(
+                  children: const [
+
+                    Icon(
+                      Icons.favorite_border,
+                      size: 38,
+                    ),
+
+                    SizedBox(height: 25),
+
+                    Icon(
+                      Icons.chat_bubble_outline,
+                      size: 38,
+                    ),
+
+                    SizedBox(height: 25),
+
+                    Icon(
+                      Icons.share,
+                      size: 38,
+                    ),
+                  ],
+                ),
+              ),
+
+              const Positioned(
+                left: 15,
+                bottom: 30,
+                child: Text(
+                  "@PitokTube",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
